3ace435778552eed5287161b268f953c2d196980
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 01:11:49 +0100

    Add files via upload

---
+++ main/java/com/example/czutbfairoomdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.czutbfairoomdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.core.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
