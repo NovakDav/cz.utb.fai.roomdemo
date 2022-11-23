a55099702090c880f3694f0b866a84c364d99a9d
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 03:02:59 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/czutbfairoomdemo/MainActivity.kt
+++ app/src/main/java/com/example/czutbfairoomdemo/MainActivity.kt
@@ -12,11 +12,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.project.core.R
 
 class MainActivity : AppCompatActivity() {
-    
+
+    private val newWordActivityRequestCode = 1
     private val wordViewModel: WordViewModel by viewModels {
         WordViewModelFactory((application as WordsApplication).repository)
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,18 +26,20 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-        
+
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, NewWordActivity::class.java)
             startActivityForResult(intent, newWordActivityRequestCode)
         }
-    
-    wordViewModel.allWords.observe(owner = this) { words ->
-            // Update the cached copy of the words in the adapter.
-            words.let { adapter.submitList(it) }
+
+
+        
+        wordViewModel.allWords.observe( this) { words ->words.let { adapter.submitList(it) }
         }
-override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
         super.onActivityResult(requestCode, resultCode, intentData)
 
         if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
