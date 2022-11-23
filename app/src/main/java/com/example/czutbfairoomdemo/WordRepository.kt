4d742d61f29f2ef5258de72666f36ec09e2092ae
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 01:42:30 +0100

    Update WordRepository.kt

--- app/src/main/java/com/example/czutbfairoomdemo/WordRepository.kt
+++ app/src/main/java/com/example/czutbfairoomdemo/WordRepository.kt
@@ -1,4 +1,16 @@
 package com.example.czutbfairoomdemo
 
-class WordRepository {
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
 }
