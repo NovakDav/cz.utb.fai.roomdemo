d5602e1d78eefda75dd0758a124ed5362eb417f3
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 02:57:11 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/czutbfairoomdemo/MainActivity.kt
+++ app/src/main/java/com/example/czutbfairoomdemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.czutbfairoomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.core.R
 
 class MainActivity : AppCompatActivity() {
+    
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
