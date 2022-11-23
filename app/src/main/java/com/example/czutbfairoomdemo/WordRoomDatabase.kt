7f0782c90de7a76a1ac82f567e94f35b4d790891
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 02:44:11 +0100

    Update WordRoomDatabase.kt

--- app/src/main/java/com/example/czutbfairoomdemo/WordRoomDatabase.kt
+++ app/src/main/java/com/example/czutbfairoomdemo/WordRoomDatabase.kt
@@ -9,7 +9,33 @@ import kotlinx.coroutines.CoroutineScope
 public abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
+    
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
 
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var wordDao = database.wordDao()
+
+                    // Delete all content here.
+                    wordDao.deleteAll()
+
+                    // Add sample words.
+                    var word = Word("Hello")
+                    wordDao.insert(word)
+                    word = Word("World!")
+                    wordDao.insert(word)
+
+                    // TODO: Add your own words!
+                    word = Word("TODO!")
+                    wordDao.insert(word)
+                }
+            }
+        }
+    }
     companion object {
         // Singleton prevents multiple instances of database opening at the
         // same time.
