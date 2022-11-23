b619b2a9cbe57d7ef63ed0ebcc1c59888f0544b0
NovakDav <58558823+NovakDav@users.noreply.github.com>
2022-11-23 01:30:28 +0100

    Update WordDao.kt

--- app/src/main/java/com/example/czutbfairoomdemo/WordDao.kt
+++ app/src/main/java/com/example/czutbfairoomdemo/WordDao.kt
@@ -1,14 +1,15 @@
+package com.example.czutbfairoomdemo
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.czutbfairoomdemo.Word
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WordDao {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): List<Word>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
